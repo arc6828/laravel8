7075e6b3570377591208092651482a1b7f1bdad8
Chavalit Koweerawong <chavalit.kow@gmail.com>
2021-07-23 12:38:23 +0700

    Week5

--- app/Http/Controllers/MyProfileController.php
+++ app/Http/Controllers/MyProfileController.php
@@ -45,18 +45,32 @@ class MyProfileController extends Controller
         return view("myprofile/show", compact('profile', 'others'));
     }
 
-    public function gallery(){
+    public function gallery()
+    {
         $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
         $bird = "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2019/04/captain_america-civil_war-anthony_mackie-photofest-h_2019.jpg";
         $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
         $god = "https://amp.insider.com/images/5b7acee73cccd122008b45ac-750-563.jpg";
         $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg";
-    
+
         return view("test/index", compact("ant", "bird", "cat", "god", "spider"));
     }
 
-    public function ant(){
-        $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";    
+    public function ant()
+    {
+        $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
         return view("test/ant", compact("ant"));
+    }
+
+    public function coronavirus()
+    {
+        $reports = [
+            (object) ["country" => "Thailand", "date" => "2020-04-19", "total" => "2765", "active" => "790", "death" => "47", "recovered" => "1928"],
+            (object) ["country" => "China", "date" => "2020-04-18", "total" => "2733", "active" => "899", "death" => "47", "recovered" => "1787"],
+            (object) ["country" => "China", "date" => "2020-04-17", "total" => "2700", "active" => "964", "death" => "47", "recovered" => "1689"],
+            (object) ["country" => "China", "date" => "2020-04-16", "total" => "2672", "active" => "1033", "death" => "46", "recovered" => "1593"],
+            (object) ["country" => "China", "date" => "2020-04-15", "total" => "2643", "active" => "1103", "death" => "43", "recovered" => "1497"],
+        ];
+        return view("myprofile/coronavirus", compact("reports"));
     }
 }
