1c5e4ebbbc5d2f8967d45e9cb0877178bc47be25
Chavalit Koweerawong <chavalit.kow@gmail.com>
2021-08-27 12:39:11 +0700

    Week 10

--- routes/web.php
+++ routes/web.php
@@ -17,64 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/react', function () {
-    return view('react');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get("/homepage", function () {
-    return "<h1>This is home page</h1>";
-});
+require __DIR__ . '/auth.php';
 
-Route::get("/blog/{id}", function ($id) {
-    return "<h1>This is blog page : {$id}55555 </h1>";
-});
-
-Route::get("/blog/{id}/edit", function ($id) {
-    return "<h1>This is blog page : {$id} for edit</h1>";
-});
-
-Route::get("/product/{a}/{b}/{c}", function ($a, $b, $c) {
-    return "<h1>This is product page </h1><div>{$a} , {$b}, {$c}</div>";
-});
-
-Route::get("/category/{a?}", function ($a = "mobile") {
-    return "<h1>This is category page : {$a} </h1>";
-});
-
-Route::get("/myorder/create", function () {
-    return "<h1>This is order form " . request("id") . " page : " . request("username") . "</h1>";
-});
-
-Route::get("/hello", function () {
-    return view("hello");
-});
-
-Route::get('/greeting', function () {
-    $name = 'James';
-    $last_name = 'Mars';
-    return view('greeting', compact('name', 'last_name'));
-});
-
-Route::get("/gallery", function () {
-    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-    $bird = "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2019/04/captain_america-civil_war-anthony_mackie-photofest-h_2019.jpg";
-    $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
-    $god = "https://amp.insider.com/images/5b7acee73cccd122008b45ac-750-563.jpg";
-    $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg";
-
-    return view("test/index", compact("ant", "bird", "cat", "god", "spider"));
-});
-
-Route::get("/gallery/ant", function () {
-    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-    
-    return view("test/ant", compact("ant"));
+Route::middleware(['auth','role:admin,teacher'])->group(function () {
+    Route::get("/teacher", function () {
+        return view("teacher");
+    });
 });
 
 
-Route::get("/teacher", function () {
-    return view("teacher");
-});
 
 Route::get("/student", function () {
     return view("student");
@@ -88,15 +43,16 @@ use App\Http\Controllers\MyProfileController;
 
 Route::get("/myprofile/create", [MyProfileController::class, "create"]);
 Route::get("/myprofile/{id}/edit", [MyProfileController::class, "edit"]);
-Route::get("/myprofile/{id}", [ MyProfileController::class , "show" ]);
+Route::get("/myprofile/{id}", [MyProfileController::class, "show"]);
 
-Route::get( "/newgallery" , [ MyProfileController::class , "gallery" ] );
-Route::get( "/newgallery/ant" , [ MyProfileController::class , "ant" ] );
+Route::get("/newgallery", [MyProfileController::class, "gallery"]);
+Route::get("/newgallery/ant", [MyProfileController::class, "ant"]);
 
-Route::get( "/coronavirus" , [ MyProfileController::class , "coronavirus" ] );
+Route::get("/coronavirus", [MyProfileController::class, "coronavirus"]);
 
 use App\Http\Controllers\Covid19Controller;
-Route::resource('/covid19', Covid19Controller::class );
+
+Route::resource('/covid19', Covid19Controller::class);
 
 // Route::get("/covid19/create",[ Covid19Controller::class , "create" ]);
 // Route::get("/covid19/{id}/edit", [ Covid19Controller::class , "edit" ]);
@@ -108,7 +64,8 @@ Route::resource('/covid19', Covid19Controller::class );
 
 
 use App\Http\Controllers\StaffController;
-Route::resource('/staff', StaffController::class );
+
+Route::resource('/staff', StaffController::class);
 
 // Route::resource('post', 'PostController'); //ยกเลิกการใช้งานเพราะเป็นวิธีการ v.6 / v.7
 use App\Http\Controllers\PostController;  //เขียนเพิ่ม
