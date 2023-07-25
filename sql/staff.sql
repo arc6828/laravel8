9793ceb72acef68841805296e34bce5a102ba2cb
Chavalit Koweerawong <chavalit.kow@gmail.com>
2023-07-25 16:11:31 +0700

    Update staff.sql

--- sql/staff.sql
+++ sql/staff.sql
@@ -1,47 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jul 25, 2023 at 09:08 AM
--- Server version: 8.0.17
--- PHP Version: 7.3.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `laravel`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `staff`
---
-
-CREATE TABLE `staff` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `birthdate` date DEFAULT NULL,
-  `salary` double(8,2) DEFAULT NULL,
-  `photo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `phone` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `staff`
---
 
 INSERT INTO `staff` (`id`, `created_at`, `updated_at`, `title`, `birthdate`, `salary`, `photo`, `phone`) VALUES
 (1, NULL, NULL, 'Qui tempore pariatur.', '2000-01-01', 2000.00, 'https://raw.githubusercontent.com/arc6828/laravel8/main/public/img/book-1.png', '099-999-9999'),
@@ -57,28 +13,3 @@ INSERT INTO `staff` (`id`, `created_at`, `updated_at`, `title`, `birthdate`, `sa
 (11, NULL, NULL, 'Perspiciatis velit est.', '2000-01-01', 2000.00, 'https://raw.githubusercontent.com/arc6828/laravel8/main/public/img/book-11.png', '099-999-9999'),
 (12, NULL, NULL, 'Ut eaque ut.', '2000-01-01', 2000.00, 'https://raw.githubusercontent.com/arc6828/laravel8/main/public/img/book-12.png', '099-999-9999'),
 (13, NULL, NULL, 'Et ut odit.', '2000-01-01', 2000.00, 'https://raw.githubusercontent.com/arc6828/laravel8/main/public/img/book-13.png', '099-999-9999');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `staff`
---
-ALTER TABLE `staff`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `staff`
---
-ALTER TABLE `staff`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
